073845ee2a1491014b28f0f441ae9506350e1708
Joshna <nadakuditijoshna5@gmail.com>
2026-06-13 11:55:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function addToCart(productName, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({
+        name: productName,
+        price: price
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(productName + " added to cart!");
+}
